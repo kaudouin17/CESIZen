2b8170c7166912902ca7f806f37c049b305ca02c
Kilian <kilian.audouin@sfr.fr>
2025-09-03 16:48:07 +0200

    database

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -26,10 +26,10 @@ class Database extends Config
      */
     public array $default = [
         'DSN'      => '',
-        'hostname' => '127.0.0.1',
+        'hostname' => 'mysql-f3b_.railway.internal',
         'username' => 'root',
-        'password' => '',
-        'database' => 'cesizen',
+        'password' => 'RGWOuFhVkIleVjSegAEQZLkUqjXLiGYo',
+        'database' => 'railway',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
