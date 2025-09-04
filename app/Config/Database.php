8f615d84b26688af79a485b2dadd604d6247d13f
Kilian <kilian.audouin@sfr.fr>
2025-09-04 10:47:04 +0200

    db

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -24,9 +24,26 @@ class Database extends Config
      *
      * @var array<string, mixed>
      */
-public array $default = [
-    'DSN'      => 'mysql://root:RGWOuFhVkIleVjSegAEQZLkUqjXLiGYo@switchback.proxy.rlwy.net:54588/railway',
-];
+    public array $production  = [
+        'DSN'          => '',
+        'hostname'     => 'switchback.proxy.rlwy.net',
+        'username'     => 'root',
+        'password'     => 'RGWOuFhVkIleVjSegAEQZLkUqjXLiGYo',
+        'database'     => 'cesizen',
+        'DBDriver'     => 'MySQLi',
+        'DBPrefix'     => '',
+        'pConnect'     => false,
+        'DBDebug'      => true,
+        'charset'      => 'utf8',
+        'DBCollat'     => 'utf8_general_ci',
+        'swapPre'      => '',
+        'encrypt'      => false,
+        'compress'     => false,
+        'strictOn'     => false,
+        'failover'     => [],
+        'port'         => 54588,
+        'numberNative' => false,
+    ];
 
     //    /**
     //     * Sample database connection for SQLite3.
