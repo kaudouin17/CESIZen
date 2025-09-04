625c8bebb909f963814db8142041a04445df6501
Kilian <kilian.audouin@sfr.fr>
2025-09-04 10:07:53 +0200

    mysqli

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -25,25 +25,24 @@ class Database extends Config
      * @var array<string, mixed>
      */
 public array $default = [
-    'DSN'        => '', // vide si tu utilises hostname/username/password
-    'hostname'   => 'switchback.proxy.rlwy.net',
-    'username'   => 'root',
-    'password'   => 'RGWOuFhVkIleVjSegAEQZLkUqjXLiGYo',
-    'database'   => 'railway',
-    'DBDriver'   => 'MySQLi',
-    'DBPrefix'   => '',
-    'pConnect'   => false,
-    'DBDebug'    => true,            // mets false en prod si tu veux
-    'charset'    => 'utf8mb4',
-    'DBCollat'   => 'utf8mb4_general_ci',
-    'swapPre'    => '',
-    'encrypt'    => false,
-    'compress'   => false,
-    'strictOn'   => false,
-    'failover'   => [],
-    'port'       => 54588,           // <-- le port public
+    'DSN'      => 'MySQLi://root:RGWOuFhVkIleVjSegAEQZLkUqjXLiGYo@mysql-f3b_.railway.internal:3306/railway',
+    'hostname' => '',
+    'username' => '',
+    'password' => '',
+    'database' => '',
+    'DBDriver' => 'MySQLi',
+    'DBPrefix' => '',
+    'pConnect' => false,
+    'DBDebug'  => true,
+    'charset'  => 'utf8mb4',
+    'DBCollat' => 'utf8mb4_general_ci',
+    'swapPre'  => '',
+    'encrypt'  => false,
+    'compress' => false,
+    'strictOn' => false,
+    'failover' => [],
+    'port'     => 3306,
 ];
-
 
     //    /**
     //     * Sample database connection for SQLite3.
